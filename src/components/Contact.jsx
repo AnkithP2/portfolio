9d96380b7e60e0489ff2df3b6f7b88789ba273bb
ankithpillay <ankithpillay@tamu.edu>
2025-03-23 17:20:16 -0500

    formatting plus some coding

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,10 @@
 const Contact = () => {
-    return (
-        <div id = "contact" className = "section">
-            <h1>Contact</h1>
-            <p>Here is my contact information</p>
-        </div>
-    );
-}
+  return (
+    <div id="contact" className="section">
+      <h1>Contact</h1>
+      <p>Here is my contact information</p>
+    </div>
+  );
+};
 
 export default Contact;
